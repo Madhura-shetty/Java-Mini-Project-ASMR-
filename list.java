e9ef0cbe6d10da40e9e29a297a98690eee1686fd
Madhura-shetty <shettymadhura26@gmail.com>
2024-10-23 20:26:53 +0530

    Update list.java

--- list.java
+++ list.java
@@ -1,5 +1,11 @@
 package classess;
+import javax.swing.JPanel;
+public class list extends JPanel{
+  list(){
+    GridLayout layout = new GridLayout(10,1);
+    layout.setVgap(5);
+    
+    this.setLayout(layout);
 
-public class list {
-
+ }
 }
