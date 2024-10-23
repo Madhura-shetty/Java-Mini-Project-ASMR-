081f20ecea712fe21f6bfaac59314ad792998f55
Madhura-shetty <shettymadhura26@gmail.com>
2024-10-23 21:00:14 +0530

    Update task.java

--- task.java
+++ task.java
@@ -1,5 +1,31 @@
 package classess;
 
-public class task {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Task extends JPanel {
+  
+  private JLabel index;
+  private JTextfield taskName;
+  private JButton done;
+
+  private boolean checked;
+  //constructor
+  Task()
+  {
+    this.setPreferredsize(new Dimension(40,20));
+    this.setBackground(Color.red);
+
+    this.setLayout(new BorderLayout()):
+
+      checked = false;
+
+    index = new JLabel()
 
 }
